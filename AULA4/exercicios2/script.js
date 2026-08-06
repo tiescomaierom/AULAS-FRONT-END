ece49349c4ff4f2807ed88c7c5f35cf1c2c9a4fd
tiescomaierom <francesco.antoniomaierom@gmail.com>
2026-08-06 20:50:06 -0300

    exercicios2

---
+++ AULA4/exercicios2/script.js
@@ -0,0 +1,64 @@
+// function (numeros) {
+//     const resultado = []
+//     for (let i = 0; i < numeros.length; i++) {
+//         if (numeros[i] % 5 == 0) {
+//             resultado.push("xazam")
+//         } else if (numeros[i] % 2 == 0) {
+//             resultado.push("par")
+//         } else {
+//             resultado.push("impar")
+//         }
+//     }
+// }
+
+
+// console.log(numeros,'numeros')
+// console.log(resultado,'resultado')
+
+
+
+
+
+
+///////////////////////////////////////////////////////
+
+// function palavraComA(palavra){
+//     let palavraAtual = []
+//     iniciam_com_a = []
+//     for(let i = 0; i <palavra.length; i++){
+//     const palavrAtual = palavra[i]
+//     if(palavraAtual[0] == "a" ){
+//         iniciam_com_a.push(palavrAtual)
+//     }else {
+//         console.log("[]")
+//     }
+//     return palavrAtual
+// }
+
+// }
+// console.log(palavraComA(["joao","maria","andre"]))
+
+
+
+///////////////////////////////////////////////////////
+
+// function inversos(nomes) {
+//     let invertidos = []
+//     for(let i = nomes.length-1; i>=0;i--){
+//         invertidos.push(nomes[i])
+//     }
+//     return invertidos
+// }
+// console.log(inversos(["joao","maria","andre"]))
+
+///////////////////////////////////////////////////////
+
+
+// function listaMultiplicacao(numeros){
+//     let total = numeros[0];
+//     for(let i =0;i<numeros.length;i++){
+//      const resultado = total * numeros[i]
+//      return resultado;
+//  }
+// }
+// console.log(listaMultiplicacao([2,2]))
