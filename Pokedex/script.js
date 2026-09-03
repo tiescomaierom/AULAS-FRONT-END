22689c00a819772cd6fe8197699b3fbcb8a5ba6d
Tiescomaierom <francesco.antoniomaierom@gmail.com>
2026-09-03 06:35:37 -0300

    alteracao cards

--- Pokedex/script.js
+++ Pokedex/script.js
@@ -12,5 +12,37 @@ async function buscarPokemons(url) {
 
     urlProximo = dados.next 
     urlAnterior = dados.previous
+
+    urlProximo = dados.next;
+    urlAnterior = dados.previous;
+    
+    // Ativa/Desativa botões
+    btnVoltar.disabled = !urlAnterior;
+    btnProximo.disabled = !urlProximo;
+
+    // Limpa o container antes de carregar os novos
+    container.innerHTML = '';
+
+    // 3. Busca os detalhes de cada Pokémon (fotos, tipos, etc)
+    const promessas = dados.results.map(pokemon => fetch(pokemon.url).then(res => res.json()));
+    const detalhesPokemons = await Promise.all(promessas);
+
+    // 4. Renderiza na tela
+    detalhesPokemons.forEach(criarCard);
 }
 buscarPokemons()
+function criarCard(pokemon) {
+    const card = document.createElement('div');
+    card.className = 'card';
+
+    // A imagem oficial fica dentro de sprites.front_default
+    const imagem = pokemon.sprites.other['official-artwork'].front_default || pokemon.sprites.front_default;
+    
+    card.innerHTML = `
+        <img src="${imagem}" alt="${pokemon.name}">
+        <h3>#${pokemon.id} - ${pokemon.name}</h3>
+        <p>Tipo: ${pokemon.types.map(tipo => tipo.type.name).join(', ')}</p>
+    `;
+
+    container.appendChild(card);
+}
